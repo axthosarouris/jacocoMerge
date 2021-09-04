d1f1ca58132b5ed5f7362e8fb36dd847c325f077
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-09-04 17:20:23 +0200

    update structure

---
+++ testProject01/moduleB/src/main/java/com/example/ExampleClass02.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class ExampleClass02 {
+
+    public ExampleClass02(){
+        super();
+    }
+
+    public int exampleMethod02(){
+        int x= 1;
+        int y= 2;
+        return x+y;
+    }
+
+}
